df8a6fdb2f0a2c4d8717d6945d2f36469642b0b3
jana-ii <142666677+jana-ii@users.noreply.github.com>
2023-09-06 20:00:24 +0300

    cw6

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # write your code here
+
+
+#len for part 2 last one
+
+person = {'name':'jana',
+'age':14,
+'hobbies':['gaming','drawing','math','piano']
+}
+
+print(person['name'])
+print(person['age'])
+
+person['age']= 17
+person['country']=['finland']
+print(person)
+print(len(person))
+
+person['hobbies'].append('bake')
+print(person)
+
+def check_hobby(person):
+    if len(person['hobbies']) >=3 :
+       print ('wow,you are amazing')
+    else:
+        print('add more hobbies')
+
+
+check_hobby(person)
